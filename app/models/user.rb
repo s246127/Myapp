955780d50b1e7ce505963286658bd6bffb4ac556
s246127 <hiroki71027923@icloud.com>
2021-01-18 21:40:57 +0900

    ユーザー認証の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+
+  has_many :teams, through: :team_users
+  has_many :posts
+  has_many :comments
+  has_many :team_users
 end
